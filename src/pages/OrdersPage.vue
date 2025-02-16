0c095227ff0552a157323f387ae8fc2ac65ea6c9
Eugenem84 <medov-foto@mail.ru>
2025-02-16 13:54:03 +0300

    причесал отображение ордеров

--- src/pages/OrdersPage.vue
+++ src/pages/OrdersPage.vue
@@ -64,14 +64,42 @@ onMounted(() => {
 </script>
 
 <template>
-  <q-page class="q-pa-md">
+  <q-page class="q-pa-none">
     <q-list bordered separator>
       <q-item-label v-if="orders.length === 0">Нет данных</q-item-label>
-      <q-item v-for="order in orders" :key="order.id" clickable v-ripple @click="goToOrderDetails">
-        <q-item-section>
-          <q-item-label>№: {{ order.id }}</q-item-label>
-          <q-item-label>Статус: {{ order.status }}</q-item-label>
-          <q-item-label>Сумма: {{ order.total_amount }}</q-item-label>
+      <q-item v-for="order in orders"
+              :key="order.id"
+              clickable
+              v-ripple
+              @click="goToOrderDetails"
+              class="flex-direction: column"
+      >
+        <!-- Первый ряд (3 колонки) -->
+        <q-item-section class="col-12">
+          <div class="row no-wrap">
+            <div class="col-6 text-left">
+              <q-item-label class="text-body1">
+                {{ new Date(order.created_at).toLocaleDateString('ru-RU') }}
+              </q-item-label>
+            </div>
+            <div class="col-4 text-center">
+              <q-item-label>
+                №: {{ order.id }}
+              </q-item-label>
+            </div>
+            <div class="col-2 text-right">
+              <q-item-label class="text-body1">
+                {{ order.total_amount }}
+              </q-item-label>
+            </div>
+          </div>
+        </q-item-section>
+
+        <!-- Второй ряд (имя клиента на всю ширину) -->
+        <q-item-section class="col-12 text-center text-gray-8 text-caption">
+          <q-item-label>
+            {{ order.client_name }}
+          </q-item-label>
         </q-item-section>
       </q-item>
     </q-list>
