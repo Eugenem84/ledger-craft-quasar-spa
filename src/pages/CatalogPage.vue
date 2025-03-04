cb5cf3551c89c574fc09e364d28c3a7df4650a7c
Eugenem84 <medov-foto@mail.ru>
2025-03-04 21:33:23 +0300

    создал диалоговое окно просмотра клиента. Не до конца реализовано редактирование

--- src/pages/CatalogPage.vue
+++ src/pages/CatalogPage.vue
@@ -7,9 +7,14 @@ const specializationStore = useSpecializationsStore()
 
 const tab = ref('services')
 
+const editCientMode = ref(false)
+
 const clients = ref([])
+const selectedClient = ref(null)
 
 const selectedSpecializationId = specializationStore.getSelectedSpecialization.id
+
+const showClientsDetails= ref(false)
 
 onMounted(() => {
   getClients()
@@ -22,6 +27,25 @@ const getClients = async () => {
     console.log('clients: ', clients.value)
   } catch (err) {
     console.error('ошибка получения клиентов: ', err)
+  }
+}
+
+const openClientDialog = (client) => {
+  selectedClient.value = {...client}
+  showClientsDetails.value = true
+}
+
+const editClient = async () => {
+  try {
+    console.log('отправление запроса на редактирование')
+    const response = await api.post(`/edit_client`, {
+      name: selectedClient.value.name,
+      phone: selectedClient.value.phone
+    })
+    console.log('response: ', response)
+    await getClients()
+  } catch (err){
+    console.error(err)
   }
 }
 
@@ -58,9 +82,11 @@ const getClients = async () => {
 
             <q-item-label v-if="!clients">Нет материалов</q-item-label>
             <q-item v-for="client in clients"
-                    :key="client"
+                    :key="client.id"
                     class="w-100 justify-between row"
                     style="width: 100%"
+                    clickable
+                    @click="openClientDialog(client)"
             >
 
               <q-item-section class="col-auto">
@@ -85,6 +111,40 @@ const getClients = async () => {
     </q-card>
 
   </q-page>
+
+  <div>
+    <q-dialog v-model="showClientsDetails" persistent>
+      <q-card>
+        <q-card-section>
+          <div class="text-h6">клиент</div>
+          <q-input :disable="!editCientMode"
+                   v-model="selectedClient.name"
+                   label-color="yellow"
+                   color="yellow"
+                   label="Имя клиента"
+                   outlined
+                   class="q-mb-md"
+          />
+          <q-input :disable="!editCientMode"
+                   v-model="selectedClient.phone"
+                   label-color="yellow"
+                   color="yellow"
+                   label="телефон"
+                   outlined class="q-mb-md"
+          />
+
+        </q-card-section>
+        <q-card-actions align="right">
+          <q-btn v-if="editCientMode" flat label="отмена" color="yellow" @click="editCientMode = false" />
+          <q-btn v-if="!editCientMode" flat label="закрыть" color="yellow" @click="showClientsDetails = false" />
+          <q-btn v-if="!editCientMode" flat label="редактировать" color="yellow" @click="editCientMode = true" />
+          <q-btn v-if="editCientMode" flat label="сохранить" color="yellow" @click="editClient" />
+          <q-btn v-if="!editCientMode" flat label="удалить" color="yellow" @click="console.log('не реализовано')" />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+  </div>
+
 </template>
 
 <style scoped>
