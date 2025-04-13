bbca02277d12bba8ef2233c72228ce5a97ec49f8
Eugenem84 <medov-foto@mail.ru>
2025-04-13 22:41:13 +0300

    исправил баг создания товара

--- src/pages/dialogs/ProductDialogPage.vue
+++ src/pages/dialogs/ProductDialogPage.vue
@@ -32,11 +32,14 @@ const open = (product, productCategory) => {
   currentProductCategory.value = productCategory || null
   name.value = product?.name || ''
   baseSalePrice.value = product?.base_sale_price || ''
-  if (currentProductCategory.value){
+  if (product){
+    console.log('product: ', product)
+    editMode.value = false
+    newProductMode.value = false
+  } else {
+    console.log('product:: ', product)
     editMode.value = true
     newProductMode.value = true
-  } else {
-    editMode.value = false
   }
   showDialog.value = true
   console.log('currentProductCategory:', currentProductCategory.value)
@@ -48,9 +51,9 @@ const open = (product, productCategory) => {
 
 const saveProduct = async () => {
   try {
-    //console.log('currentCategoryId: ', currentCategory.value.id)
-    if (editMode.value){
-      console.log('редактирование продукта не реализовано')
+    if (!currentProductCategory.value){
+      console.log('currenProduct: ', currentProduct.value.id)
+      console.log('редактирование продукта...')
       await api.post(`/edit_product`, {
         id: currentProduct.value.id,
         name: name.value,
